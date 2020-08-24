9ea39bedd5ac38d58929bbe3e03dc78ac1504ba0
Dzirt2006 <dzirt-2006@mail.ru>
2020-08-23 22:14:38 -0400

    deploy version

--- server/auth/google.js
+++ server/auth/google.js
@@ -1,77 +1,54 @@
 const passport = require('passport')
 const router = require('express').Router()
 const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy
-const { User,Pool } = require('../db/models')
-const secrets = require('./secret');
+const { User } = require('../db/models')
 module.exports = router
 
-/**
- * For OAuth keys and other secrets, your Node process will search
- * process.env to find environment variables. On your production server,
- * you will be able to set these environment variables with the appropriate
- * values. In development, a good practice is to keep a separate file with
- * these secrets that you only share with your team - it should NOT be tracked
- * by git! In this case, you may use a file called `secrets.js`, which will
- * set these environment variables like so:
- *
- * process.env.GOOGLE_CLIENT_ID = 'your google client id'
- * process.env.GOOGLE_CLIENT_SECRET = 'your google client secret'
- * process.env.GOOGLE_CALLBACK = '/your/google/callback'
- */
 
-// if (!process.env.clientID || !process.env.clientSecret ) {
-//     console.log('Google client ID / secret not found. Skipping Google OAuth.')
-// } else {
-//     const googleConfig = {
-//         clientID: process.env.GOOGLE_CLIENT_ID,
-//         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-//         callbackURL: process.env.GOOGLE_CALLBACK
-//     }
-    const googleConfig = {
-        clientID: secrets.google.clientID,
-        clientSecret: secrets.google.clientSecret,
-        callbackURL: secrets.google.callbackURL
-    }
-   
+const googleConfig = {
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: process.env.GOOGLE_CALLBACK
+}
 
-    const strategy = new GoogleStrategy(
-        googleConfig,
-        async (token, refreshToken, profile, done) => {
-            const googleId = profile.id
-            const email = profile.emails[0].value
-            const imgUrl = profile.photos[0].value
-            const userName = profile.displayName
+
+const strategy = new GoogleStrategy(
+    googleConfig,
+    async (token, refreshToken, profile, done) => {
+        const googleId = profile.id
+        const email = profile.emails[0].value
+        const imgUrl = profile.photos[0].value
+        const userName = profile.displayName
 
         try {
             const [user] = await User.findOrCreate({
                 where: { googleId },
-                defaults: { email, userName,imgUrl  }
+                defaults: { email, userName, imgUrl }
             })
             done(null, user) // the user we pass to done here is piped through passport.serializeUser
-          } catch (err) {
+        } catch (err) {
             done(err)
-          }
         }
-    )
+    }
+)
 
-    passport.use(strategy)
+passport.use(strategy)
 
-    router.get('/', passport.authenticate('google', { scope: ['email', 'profile'] })
-    )
+router.get('/', passport.authenticate('google', { scope: ['email', 'profile'] })
+)
 
-    router.get('/callback',
-        passport.authenticate('google', {
-            successRedirect: '/home',
-            failureRedirect: '/'
-        })
-    )
-    router.delete('/logout', (req, res) => {
-        req.logout()
-        req.session.destroy()
+router.get('/callback',
+    passport.authenticate('google', {
+        successRedirect: '/home',
+        failureRedirect: '/'
     })
+)
+router.delete('/logout', (req, res) => {
+    req.logout()
+    req.session.destroy()
+})
 
-    router.get('/me', (req, res) => {
-        console.log('me')
-        res.json(req.user)
-    })
-// }
+router.get('/me', (req, res) => {
+    res.json(req.user)
+})
+
