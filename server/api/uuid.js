f02c433e6a7f9acf31a3892b6f6873ca90519000
Dzirt2006 <dzirt-2006@mail.ru>
2020-08-25 22:31:51 -0400

    added uuid model and api

---
+++ server/api/uuid.js
@@ -0,0 +1,30 @@
+const router = require('express').Router();
+const { UUID,Pool,User } = require('../db/models');
+module.exports = router;
+
+router.post('/new_uuid', async (req, res, next) => {
+    try {
+        await UUID.create(req.body)
+            .then(() => res.sendStatus(200));
+    } catch (err) {
+        next(err);
+    }
+
+})
+
+
+router.post('/use_uuid', async (req, res, next) => {
+    const user = req.user;
+    const  uuidInpt = req.body
+    try {
+        const {refId} = await UUID.findOne({where:{uuid:uuidInpt}})
+        const pool = await Pool.findOne({ where: { id: refId.id } });
+        await User.findOne({ where: { googleId: user.googleId } })
+            .then(user => user.addPool(pool))
+        data = await User.findOne({ where: { googleId: user.googleId }, include: [Pool] })
+        res.json(data);
+    } catch (err) {
+        next(err);
+    }
+})
+
