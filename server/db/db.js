01d49bea57a72f12e4c9ac48d2fcbe20d1d8fa9d
Dzirt2006 <dzirt-2006@mail.ru>
2021-03-23 20:27:40 -0400

    db ssl on3

--- server/db/db.js
+++ server/db/db.js
@@ -4,7 +4,7 @@ const pkg = require('../../package.json')
 const databaseName = pkg.name;
 
 const db = new Sequelize(
-    process.env.DATABASE_URL+"?ssl=true" ||
+    process.env.DATABASE_URL+"?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory" ||
     `postgres://postgres:w2w2@localhost:5432/${databaseName}`, //
     {
         logging: false
